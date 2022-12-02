391d252f727865e217831ed94364f10c38cd6328
01001100 <datby88@gmail.com>
2022-12-02 18:28:37 +1100

    kinda works

--- src/settings.h
+++ src/settings.h
@@ -2,15 +2,15 @@
 #define SETTINGS_H
 
 //Weather Settings
-#define CITY_ID "2755420" //Gouda https://openweathermap.org/current#cityid
-#define OPENWEATHERMAP_APIKEY "[get your key at openweathermap]" //use your own API key :)
+#define CITY_ID "2172517" //Gouda https://openweathermap.org/current#cityid
+#define OPENWEATHERMAP_APIKEY "d7be3f04c58848f611c63ba2830f8e76" //use your own API key :)
 #define OPENWEATHERMAP_URL "http://api.openweathermap.org/data/2.5/weather?id=" //open weather api
 #define TEMP_UNIT "metric" //metric = Celsius , imperial = Fahrenheit
 #define TEMP_LANG "en"
 #define WEATHER_UPDATE_INTERVAL 30 //must be greater than 5, measured in minutes
 //NTP Settings
 #define NTP_SERVER "pool.ntp.org"
-#define GMT_OFFSET_SEC 3600 * +2 //New York is UTC -5
+#define GMT_OFFSET_SEC 3600 * +11 //New York is UTC -5
 #define DST_OFFSET_SEC 3600
 
 watchySettings settings{
